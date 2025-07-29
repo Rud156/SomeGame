8e9d656151e38bcf13efb440e2495b67abd9edcf
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2025-07-29 07:59:51 -0400

    Setup Haxe

---
+++ src/test/Test.hx
@@ -0,0 +1,13 @@
+package src.test;
+
+import godot.*;
+
+class Test extends Node
+{
+    public override function _ready(){
+        trace("Hello World");
+    }
+
+    public override function _input(event:InputEvent){
+    }
+}
