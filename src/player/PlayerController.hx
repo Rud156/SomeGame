2357037af1b3e93919202c02f0ce961dd4181bc6
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2025-08-01 21:58:02 -0400

    Added needed assets

--- src/player/PlayerController.hx
+++ src/player/PlayerController.hx
@@ -5,6 +5,18 @@ import godot.*;
 
 class PlayerController extends CharacterBody3D {
 	// ================================
+	// Export
+	// ================================
+	@:export
+	var movementSpeed:Float;
+	@:export
+	var maxJumpCount:Int;
+
+	private var _gravity:Float;
+    private var _movementVelocity:Vector3;
+
+	// https://github.com/KenneyNL/Starter-Kit-3D-Platformer/blob/main/scripts/player.gd
+	// ================================
 	// Override Functions
 	// ================================
 	public override function _process(delta:Float):Void {}
