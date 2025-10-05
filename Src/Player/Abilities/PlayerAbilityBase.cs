09a1f0b37d7f9aeaef89dc5ad6672b230a6f8e2b
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2025-10-05 14:30:15 -0400

    Updated PlayerAbilityBase with required HUD updates

--- Src/Player/Abilities/PlayerAbilityBase.cs
+++ Src/Player/Abilities/PlayerAbilityBase.cs
@@ -7,13 +7,38 @@ namespace SomeGame.Player.Abilities
     public abstract partial class PlayerAbilityBase : AbilityBase
     {
         // ================================
+        // Ability Functions
+        // ================================
+
+        public override void Start()
+        {
+            base.Start();
+            PlayerAbilityDisplay.Instance.TriggerAbilityFx(AbilityDisplay.abilityType);
+        }
+
+        // ================================
         // Override Functions
         // ================================
 
         public override void _Ready()
         {
+            base._Ready();
+
             PlayerAbilityDisplay.Instance.SetAbilityIcon(AbilityDisplay.abilityIcon, AbilityDisplay.abilityType);
             PlayerAbilityDisplay.Instance.SetAbilityBorderColor(ExtensionFunctions.AverageColorFromTexture(AbilityDisplay.abilityIcon), AbilityDisplay.abilityType);
+        }
+
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+
+            PlayerAbilityDisplay.Instance.SetAbilityProgress(
+                currentCooldownDuration,
+                currentCooldownDuration / AbilityDisplay.cooldownDuration,
+                AbilityDisplay.abilityType
+            );
+
+            PlayerAbilityDisplay.Instance.SetAbilityStackCount(currentStackCount, AbilityDisplay.stackCount, AbilityDisplay.abilityType);
         }
     }
 }
